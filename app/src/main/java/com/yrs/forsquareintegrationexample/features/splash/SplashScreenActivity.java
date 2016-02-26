7e73f487cebbf6dac9747733e11178152397056b
yaros <yaros.come@gmail.com>
2016-02-26 09:48:36 +0000

    Images for search edittext

--- app/src/main/java/com/yrs/forsquareintegrationexample/features/splash/SplashScreenActivity.java
+++ app/src/main/java/com/yrs/forsquareintegrationexample/features/splash/SplashScreenActivity.java
@@ -1,9 +1,11 @@
 package com.yrs.forsquareintegrationexample.features.splash;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.yrs.forsquareintegrationexample.R;
+import com.yrs.forsquareintegrationexample.features.search.SearchActivity;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -11,5 +13,17 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        try {
+            Thread.sleep(3000);
+            startActivity(new Intent(this, SearchActivity.class));
+            finish();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
