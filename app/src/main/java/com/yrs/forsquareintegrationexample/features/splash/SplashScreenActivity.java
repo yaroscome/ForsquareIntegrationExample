33d862cfa9208783a8370ffb8a0dc1006df21e05
yaros <yaros.come@gmail.com>
2016-02-26 18:18:52 +0000

    Splash Screen update

--- app/src/main/java/com/yrs/forsquareintegrationexample/features/splash/SplashScreenActivity.java
+++ app/src/main/java/com/yrs/forsquareintegrationexample/features/splash/SplashScreenActivity.java
@@ -1,29 +1,37 @@
 package com.yrs.forsquareintegrationexample.features.splash;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.yrs.forsquareintegrationexample.R;
+import com.yrs.forsquareintegrationexample.base.BaseActivity;
 import com.yrs.forsquareintegrationexample.features.search.SearchActivity;
 
-public class SplashScreenActivity extends AppCompatActivity {
+public class SplashScreenActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        try {
-            Thread.sleep(3000);
-            startActivity(new Intent(this, SearchActivity.class));
-            finish();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(SplashScreenActivity.this, SearchActivity.class);
+                startActivity(intent);
+                SplashScreenActivity.this.finish();
+            }
+        }, 1200);
+
     }
 }
